e359ef673f8fe6b59fbe16fe015d8b875ac3c59e
Francisco Ortiz <pacoortiz16@hotmail.com>
2022-12-26 12:43:17 -0600

    working on Pokemon component styles

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import './styles/PokemonCard.css'
 
@@ -7,16 +8,22 @@ const PokemonCard = ({pokemon}) => {
 
   const [dataPokemon, setDataPokemon] = useState()
 
+  const types = dataPokemon?.types.map(type => type.type.name).join(" / ")
+  
+  const navigate = useNavigate()
+
+  const handleClickPokemon = () => {
+    navigate(`/pokemon/${dataPokemon?.id}`)
+  }
+  
   useEffect(() => {
     axios.get(pokemon.url)
       .then(res => setDataPokemon(res.data))
       .catch(err => console.log(err))
   }, [])
 
-  const types = dataPokemon?.types.map(type => type.type.name).join(" / ")
-
   return (
-    <article className={`pokeCard border-${dataPokemon?.types[0].type.name}`}>
+    <article onClick={handleClickPokemon} className={`pokeCard border-${dataPokemon?.types[0].type.name}`}>
       <section className={`pokeCard-header bg-lg-${dataPokemon?.types[0].type.name}`}></section>
       <section className='pokecard-content'>
         <img src={dataPokemon?.sprites.other["official-artwork"].front_default} alt="" className='pokeCard-img'/>
@@ -27,7 +34,7 @@ const PokemonCard = ({pokemon}) => {
         <section className='pokeCard-stats'>
           {
             dataPokemon?.stats.map(stat => (
-              <div className="pokeCard-stat">
+              <div key={stat.stat.name} className="pokeCard-stat">
                 <p className='pokeCard-stat-name'>{stat.stat.name}</p>
                 <p className='pokeCard-stat-value'>{stat.base_stat}</p>
               </div>
--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,8 +1,76 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import './styles/Pokemon.css'
 
 const Pokemon = () => {
+
+  const [dataPokemon, setDataPokemon] = useState()
+
+  const {id} = useParams()
+
+  useEffect(() => {
+    const URL = `https://pokeapi.co/api/v2/pokemon/${id}/`
+    axios.get(URL)
+      .then(res => setDataPokemon(res.data))
+      .catch(err => console.log(err))
+  }, [])
   return (
-    <div>Pokemon</div>
+    <main className='Pokemon'>
+      <section className='pokemonId'>
+        <section className='pokemonId-header'></section>
+        <img className='pokeminId-img' src={dataPokemon?.sprites.other["official-artwork"].front_default} alt={dataPokemon?.name} />
+        <h3 className='pokemonId-id'># {dataPokemon?.id}</h3>
+        <h2 className='pokemonId-name'>{dataPokemon?.name}</h2>
+        <section className='pokemonId-features'>
+          <div className='pokemonId-feature'>
+            <p className='pokemonId-feature-name'>Weight</p>
+            <p className='pokemonId-feature-value'>{dataPokemon?.weight}</p>
+          </div>
+          <div className='pokemonId-feature'>
+            <p className='pokemonId-feature-name'>Height</p>
+            <p className='pokemonId-feature-value'>{dataPokemon?.height}</p>
+          </div>
+        </section>
+        <section className='pokemonId-info'>
+          <div className='pokemonId-info-container'>
+            <h4 className='pokemonId-info-title'>Types</h4>
+            <div className='pokemonId-info-data'>
+              {
+                dataPokemon?.types.map(type => <p className='pokemonId-info-value' key={type.type.name}>{type.type.name}</p>)
+              }
+            </div>
+          </div>
+          <div className='pokemonId-info-container'>
+            <h4 className='pokemonId-info-title'>Skills</h4>
+            <div className='pokemonId-info-data'>
+              {
+                dataPokemon?.abilities.map(ability => <p className='pokemonId-info-value' key={ability.ability.url}>{ability.ability.name}</p>)
+              }
+            </div>
+          </div>
+        </section>
+        <section className='pokemonId-stats'>
+          <h3 className='pokemonId-stats-title'>Stats</h3>
+          <div className='pokemonId-stats-container'>
+            {
+              dataPokemon?.stats.map(stat => (
+                <div className="pokemonId-stat">
+                  <div className='pokemonId-stat-header'>
+                    <p className="pokemonId-stat-name">{stat.stat.name}</p>
+                    <p className='pokemonId-stat-value'>{stat.base_stat}</p>
+                    <div className="pokemonId-stat-bar">
+                      <div className="pokemonId-stat-barProgress"></div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </section>
+      </section>
+    </main>
   )
 }
 
